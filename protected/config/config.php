01478682bd5ae6e18797be7c162564d4f1758dec
athulkrishnan <mail@athulkrishnan.com>
2016-10-26 01:34:18 +0530

    add configs for mobile view

--- protected/config/config.php
+++ protected/config/config.php
@@ -1,7 +1,7 @@
 <?php
 // db constants
 define("mysql_host","localhost"); // database host
-define("db_name","news_app"); // database name
+define("db_name","cetaa_app"); // database name
 define("db_user", "root"); // database user
 define("db_password", "qTjzL96z"); // database password
 
