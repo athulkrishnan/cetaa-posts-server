b2cfda18c793fec81e18d3e96f0746dc10a5b558
athulkrishnan <mail@athulkrishnan.com>
2016-10-25 17:46:26 +0530

    add config

--- protected/config/config.php
+++ protected/config/config.php
@@ -1,7 +1,7 @@
 <?php
 // db constants
 define("mysql_host","localhost"); // database host
-define("db_name","news_app"); // database name
+define("db_name","cetaaposts"); // database name
 define("db_user", "root"); // database user
-define("db_password", ""); // database password
+define("db_password", "qTjzL96z"); // database password
 
--- protected/config/params.php
+++ protected/config/params.php
@@ -1,12 +1,12 @@
 <?php
 return array(
-	'site_url'=> 'http://example.com', // website url without leading /
-	'static_path'=> 'http://example.com/static/', // should be site_url/static/ // Don't change change it without changing uploading script
-	'site_name'=> 'Tiny News',
-	'site_title'=> 'Tiny News is a popular website for reading tiny news',
-	'site_description'=> 'Tiny News is a popular website for reading tiny news',
+	'site_url'=> 'http://bitnami-lampstack-d54c.cloudapp.net', // website url without leading /
+	'static_path'=> 'http://bitnami-lampstack-d54c.cloudapp.net/static/', // should be site_url/static/ // Don't change change it without changing uploading script
+	'site_name'=> 'CETAA News',
+	'site_title'=> 'News updates for CETAA App',
+	'site_description'=> 'News updates for CETAA App',
 	'post_count' => '10',
 	// misc values
-	'facebook'=> 'https://www.facebook.com/weblogtemplates',
+	'facebook'=> 'https://www.facebook.com',
 	'API_KEY'=> 'SPI_KEY',
 );
