e1b69e4e329d62807b425166e6615de7cc3c3d7d
athulkrishnan <mail@athulkrishnan.com>
2016-10-25 17:31:10 +0530

    initial commit

---
+++ protected/config/params.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+	'site_url'=> 'http://example.com', // website url without leading /
+	'static_path'=> 'http://example.com/static/', // should be site_url/static/ // Don't change change it without changing uploading script
+	'site_name'=> 'Tiny News',
+	'site_title'=> 'Tiny News is a popular website for reading tiny news',
+	'site_description'=> 'Tiny News is a popular website for reading tiny news',
+	'post_count' => '10',
+	// misc values
+	'facebook'=> 'https://www.facebook.com/weblogtemplates',
+	'API_KEY'=> 'SPI_KEY',
+);
